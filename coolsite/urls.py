c9626f4ee6038a4daa5aac6cabb8f662349364c0
vsellennaya <mk.kasiet@gmail.com>
2023-06-20 12:16:45 +0600

    added women model

--- coolsite/urls.py
+++ coolsite/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for coolsite project.
+"""coolsite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,26 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from coolsite import settings
 from women.views import *
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls), 
+    path('admin/', admin.site.urls),
     path('', include('women.urls')),
-
-    # path('women/', index), #http://127.0.0.1:8000/women/
-    # path('cats/',categories), #http://127.0.0.1:8000/cats/
-    # path('', index), #http://127.0.0.1:8000
 ]
 
-# for 404 page
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = pageNotFound
-
-# handler500 - mistake 
-# handler403 - forbidden error
-# handler400 - bad request
-
