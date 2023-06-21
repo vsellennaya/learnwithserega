dc9f997e3900a3fbdcc2f092119d1ec39ce16f56
vsellennaya <mk.kasiet@gmail.com>
2023-06-22 01:20:48 +0600

    added templates

--- women/urls.py
+++ women/urls.py
@@ -4,9 +4,12 @@ from .views import *
 
 urlpatterns = [
     path('',index, name='home'), #http://127.0.0.1:8000/
-    path('cats/<int:catid>/', categories), #http://127.0.0.1:8000/cats/1/
-    # path('cats/<slug:cat>/', categories), #http://127.0.0.1:8000/cats/1ss-s_/
+    path('about/', about, name='about'),
 
-    re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
+
+    # path('cats/<int:catid>/', categories), #http://127.0.0.1:8000/cats/1/
+    # # path('cats/<slug:cat>/', categories), #http://127.0.0.1:8000/cats/1ss-s_/
+
+    # re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
 
 ]
--- women/views.py
+++ women/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 
-
+from .models import *
 
 # Create your views here.
+
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+
+
 def index(request): #HttpRequest
-    return HttpResponse("Страница приложения women.")
+    posts = Women.objects.all()
+    return render(request, 'women/index.html', {'posts': posts, 'menu': menu, 'title': 'Главная страница' })
+
+def about(request): #HttpRequest
+    return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
+
 
 def categories(request, catid):
     if (request.GET):
