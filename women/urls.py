e98d92e50d48df45fbda3f3d7ec78ac5cec1d036
vsellennaya <mk.kasiet@gmail.com>
2023-06-22 21:37:31 +0600

    created connections between models using ForeignKey

--- women/urls.py
+++ women/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('contact/', contact, name='contact'),
     path('login/', login, name='login'),
     path('post/<int:post_id>/', show_post, name='post'),
-
+    path('category/<int:cat_id>/', show_category, name='category'),
 
 
     # path('cats/<int:catid>/', categories), #http://127.0.0.1:8000/cats/1/
