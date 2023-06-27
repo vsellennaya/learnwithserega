733714cc9b85d345251251b9d90122c6c5029695
vsellennaya <mk.kasiet@gmail.com>
2023-06-27 16:32:03 +0600

    added forms not related to models

--- women/views.py
+++ women/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 
+from .forms import *
 from .models import *
 
 # Create your views here.
@@ -28,7 +29,18 @@ def about(request): #HttpRequest
     return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
 
 def addpage(request): #HttpRequest
-    return HttpResponse('Добавление статьи')
+    if request.method == 'POST':
+        form = AddPostForm(request.POST)
+        if form.is_valid():
+            # print(form.cleaned_data)
+            try:
+                Women.objects.create(**form.cleaned_data)
+                return redirect('home')
+            except:
+                form.add_error(None, 'Ошибка добавления поста')
+    else:
+        form = AddPostForm()
+    return render(request, 'women/addpage.html', {'form': form, 'menu': menu, 'title': 'Добавление статьи'})
 
 def contact(request): #HttpRequest
     return HttpResponse('Обратная связь')
